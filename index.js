940b6d5a836f17459d5aa2f5c36da21ec570bee4
stacyvana <ssvana83@yahoo.com>
2022-09-10 10:34:28 -0400

    commit tree a9682cd803aeb043b84cdd3eb2408fdf40f4bdc6

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Write your code here!
+const element = document.getElementById('main');
+element.remove();
+
+const newHeader = document.createElement('h1');
+newHeader.id = 'victory';
+newHeader.innerHTML = "Stacy is the champion";
+
+// function addElement () {
+//   const newHeader = document.createElement('h1#victory')
+// };
+
+// var element = document.createElement("")
+// document.body.append('h1#victory');
+
